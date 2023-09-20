f8e3eef4ae0a45b2ae0293e470cc3c1c3ae76168
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-09-21 00:00:14 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+//NPM packages
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { MongoClient } = require('mongodb');
+const { default: axios } = require('axios');
+
+//js files
+const auth = require('./auth');
+
+//Code.
+const app = express();
+app.use(cors({
+  origin: ['http://localhost:5173'] // Replace with your frontend's origin
+}));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+let port = process.env.PORT
+app.listen(8000 || port);
+
+const uri = process.env.MONGO_URL;
+const client = new MongoClient(uri);
+client.connect();
+
+auth(app);
+
+function pingLink() {
+  const linkToPing = 'https://filmyradar-backend.onrender.com/movie/top_rated'; // Replace with the link you want to ping
+  let data = axios.get(linkToPing)
+  data.then(res => { })
+}
+
+// Ping the link every 10 minutes (10 minutes = 600,000 milliseconds)
+const pingInterval = 10 * 60 * 1000;
+setInterval(pingLink, pingInterval);
