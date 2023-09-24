221228cafcc01bef4a1248a72d8d93f31371644c
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-09-24 22:28:36 +0530

    trial useLocation

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,8 +9,9 @@ import { useDispatch } from "react-redux"
 import { addData } from "../Store/slice/AllPostSlice"
 import Cookies from "js-cookie"
 import { login } from "../Store/slice/userSlice"
+import { useLocation } from "react-router"
 const Home = () => {
-
+    const location = useLocation()
     const credentials = ((Cookies.get('echoChamberCred')))
     const dispatch = useDispatch()
     const [posts, setPosts] = useState([])
@@ -39,7 +40,7 @@ const Home = () => {
             }
         }
         getPosts()
-    }, [])
+    }, [location.pathname])
     
     
     useEffect(()=>{
