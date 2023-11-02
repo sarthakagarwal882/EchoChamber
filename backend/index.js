ecb24611c53afe00d59d1bc8d33bf0fe5c04adea
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-11-02 12:38:10 +0530

    backend ping update

--- backend/index.js
+++ backend/index.js
@@ -31,13 +31,13 @@ auth(app);
 post(app);
 
 function pingLink() {
-    const linkToPing = 'https://echochamber-backend-77jt.onrender.com/test'; // Replace with the link you want to ping
+    const linkToPing = 'https://echochamber-backend.onrender.com/test'; // Replace with the link you want to ping
     let data = axios.get(linkToPing)
     data.then(res => { })
 
 }
 
 // Ping the link every 11 minutes (10 minutes = 600,000 milliseconds)
-const pingInterval = 15 * 60 * 1000;
+const pingInterval = 14 * 60 * 1000;
 setInterval(pingLink, pingInterval);
 
